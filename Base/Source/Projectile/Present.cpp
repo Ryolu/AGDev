111a545b0b89923cdb65914e722633a27c115d6b
Jasper <rendescar63@gmail.com>
2017-12-13 11:54:58 +0800

    start 2.0

---
+++ Base/Source/Projectile/Present.cpp
@@ -0,0 +1,104 @@
+#include "Present.h"
+
+#include "MeshBuilder.h"
+#include "../EntityManager.h"
+#include "GraphicsManager.h"
+#include "RenderHelper.h"
+#include "GL\glew.h"
+#include "../PlayerInfo/PlayerInfo.h"
+#include "MyMath.h"
+#include "../SpatialPartition/SpatialPartition.h"
+#include "../SceneGraph/SceneGraph.h"
+
+#include <iostream>
+using namespace std;
+
+CPresent::CPresent(void)
+	: CProjectile(NULL)
+	, m_fGravity(-10.0f)
+	, m_fElapsedTime(0.0f)
+	, m_pTerrain(NULL)
+{
+}
+
+CPresent::CPresent(Mesh * _modelMesh)
+	: CProjectile(_modelMesh)
+	, m_fGravity(-10.0f)
+	, m_fElapsedTime(0.0f)
+	, m_pTerrain(NULL)
+{
+}
+
+CPresent::~CPresent(void)
+{
+	m_pTerrain = NULL; // Don't delete this as the terrain is deleted in CPlayerInfo
+	modelMesh = NULL;
+	theSource = NULL;
+}
+
+void CPresent::Update(double dt)
+{
+	if (m_bStatus == false)
+		return;
+
+	// Update TimeLife of projectile. Set to inactive if too long
+	m_fLifetime -= (float)dt;
+	if (m_fLifetime < 0.0f)
+	{
+		SetStatus(false);
+		SetIsDone(true);	// This method informs EntityManager to remove this instance
+
+							// Check the SpatialPartition to destroy nearby objects
+		vector<EntityBase*> ExportList = CSpatialPartition::GetInstance()->GetObjects(position, 1.0f);
+		for (int i = 0; i < ExportList.size(); ++i)
+		{
+			// Remove from Scene Graph
+			if (CSceneGraph::GetInstance()->DeleteNode(ExportList[i]) == true)
+			{
+				cout << "*** This Entity removed ***" << endl;
+			}
+
+
+		}
+		return;
+	}
+
+	// Check if the Grenade is already on the ground
+	if (position.y >= m_pTerrain->GetTerrainHeight(position) - 10.0f + Math::EPSILON)
+	{
+		// Update Position
+		m_fElapsedTime += dt;
+
+		position.Set(position.x + (float)(theDirection.x * m_fElapsedTime * m_fSpeed),
+			position.y + (float)(theDirection.y * m_fElapsedTime * m_fSpeed) + (0.5 * m_fGravity * m_fElapsedTime * m_fElapsedTime),
+			position.z + (float)(theDirection.z * m_fElapsedTime * m_fSpeed));
+
+		if (position.y < m_pTerrain->GetTerrainHeight(position) - 10.0f)
+		{
+			position.y = m_pTerrain->GetTerrainHeight(position) - 10.0f;
+			m_fSpeed = 0.0f;
+			return;
+		}
+	}
+}
+
+void CPresent::SetTerrain(GroundEntity * m_pTerrain)
+{
+	this->m_pTerrain = m_pTerrain;
+}
+
+CPresent * Create::Present(const std::string & _meshName, const Vector3 & _position, const Vector3 & _direction, const float m_fLifetime, const float m_fSpeed, CPlayerInfo * _source)
+{
+	Mesh* modelMesh = MeshBuilder::GetInstance()->GetMesh(_meshName);
+	if (modelMesh == nullptr)
+		return nullptr;
+
+	CPresent* result = new CPresent(modelMesh);
+	result->Set(_position, _direction, m_fLifetime, m_fSpeed);
+	result->SetStatus(true);
+	result->SetCollider(true);
+	result->SetSource(_source);
+	result->SetTerrain(_source->GetTerrain());
+	EntityManager::GetInstance()->AddEntity(result, true);
+	return result;
+}
--- Base/Source/WeaponInfo/Throw.cpp
+++ Base/Source/WeaponInfo/Throw.cpp
@@ -1,4 +1,5 @@
 #include "Throw.h"
+#include "../Projectile/Present.h"
 
 CThrow::CThrow()
 {
@@ -10,8 +11,45 @@ CThrow::~CThrow()
 
 void CThrow::Init(void)
 {
+	// Call the parent's Init method
+	CWeaponInfo::Init();
+
+	// The number of ammunition in a magazine for this weapon
+	magRounds = 50;
+	// The maximum number of ammunition for this magazine for this weapon
+	maxMagRounds = 50;
+	// The current total number of rounds currently carried by this player
+	totalRounds = 50;
+	// The max total number of rounds currently carried by this player
+	maxTotalRounds = 50;
+
+	// The time between shots
+	timeBetweenShots = 0.1;
+	// The elapsed time (between shots)
+	elapsedTime = 0.0;
+	// Boolean flag to indicate if weapon can fire now
+	bFire = true;
 }
 
 void CThrow::Discharge(Vector3 position, Vector3 target, CPlayerInfo * _source, float multiplier)
 {
+	if (bFire)
+	{
+		// If there is still ammo in the magazine, then fire
+		if (magRounds > 0)
+		{
+			// Create a projectile with a cube mesh. Its position and direction is same as the player.
+			// It will last for 3.0 seconds and travel at 500 units per second
+			CProjectile* aProjectile = Create::Present("cube",
+				position,
+				(target - position).Normalized(),
+				7.0f,
+				multiplier,
+				_source);
+			aProjectile->SetCollider(true);
+			aProjectile->SetAABB(Vector3(0.5f, 0.5f, 0.5f), Vector3(-0.5f, -0.5f, -0.5f));
+			bFire = false;
+			magRounds--;
+		}
+	}
 }
